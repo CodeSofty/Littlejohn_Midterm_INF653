e82763370b92df9d580c506b16abc44c3eba23de
CodeSoftly <eclittlejohn@yahoo.com>
2021-03-15 20:33:39 -0500

    commit

--- model/database.php
+++ model/database.php
@@ -1,13 +1,15 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
+    $dsn = 'mysql:host=g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=zsoacbo8t52k0we8';
+    $username = 'um65gkttlf3d0tct';
+    $password = 't0q4zhu1m9wz3yio';
 
-    try {
-        $db = new PDO($dsn, $username);
-
-    } catch (PDOException $e) {
-        $error = 'Database Error: ';
-        $error .= $e->getMessage();
-        include(view/error.php);
+    try
+    {
+        $db = new PDO($dsn,$username,$password);
+    } catch (PDOException $e)
+    {
+        $error = "Database error: ";
+        $error .= $e -> getMessage();
+        include('view/error.php');
         exit();
     }
