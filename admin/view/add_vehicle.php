145293173f5f990bd615779d63125cca10cbafce
CodeSoftly <eclittlejohn@yahoo.com>
2021-03-15 21:15:33 -0500

    commit

--- admin/view/add_vehicle.php
+++ admin/view/add_vehicle.php
@@ -78,8 +78,7 @@
 </section>
 
 
-
-<p><a href=".?action=add_vehicle">Add Vehicle</a></p>
+<p><a href= ".?action=list_vehicles">View Vehicles</a></p>
 <p><a href=".?action=list_types">View/Edit Types</a></p>
 <p><a href=".?action=list_makes">View/Edit Makes</a></p>
 <p><a href=".?action=list_classes">View/Edit Classes</a></p>
--- admin/view/error.php
+++ admin/view/error.php
@@ -5,7 +5,7 @@ echo $message;
 
 
 ?>
-
+<p><a href= ".?action=list_vehicles">View Vehicles</a></p>
 <p><a href=".?action=add_vehicle">Add Vehicle</a></p>
 <p><a href=".?action=list_types">View/Edit Types</a></p>
 <p><a href=".?action=list_makes">View/Edit Makes</a></p>
